b835d82c40968fb46f2331dbd12af6bdf4fbacfb
TalkoDA <azzi2710@gmail.com>
2025-07-02 20:03:25 +0200

    fixed

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,6 +1,12 @@
+import argparse
+
+
 def main():
     print('HELLO')
     print('Let us go')
+    parser = argparse.ArgumentParser()
+    parser.parse_args()
+    print('Finish')
 
 if __name__ == '__main__':
     main()
