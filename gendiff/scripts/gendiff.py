a396fa0ece94ebd511a688bbcafee977f55127d9
TalkoDA <azzi2710@gmail.com>
2025-07-06 17:40:19 +0200

    ok

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -9,6 +9,7 @@ def main():
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument("-f", "--format", help='set format of output')
+    args = parser.parse_args()
     print(f'Comparing: {args.first_file} vs {args.second_file}, format: {args.format}')
     print('Finish')
 
