cf3375433b48d968c4e3b9056d0fc8a932847a57
TalkoDA <azzi2710@gmail.com>
2025-07-23 20:17:36 +0200

    step 5

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,5 +1,17 @@
 import pytest
 
 from gendiff.scripts.generate_diff import generate_diff
-from gendiff.scripts.parser import get_file_path # ?
+from gendiff.scripts.file_parser import parse_data_from_file # ?
 
+
+@pytest.mark.parametrize('file_path1, file_path2, expected_result', [
+    ('tests/test_data/file1.json',
+     'tests/test_data/file2.json',
+     'tests/test_data/expected_result_json.txt'),
+    ('tests/test_data/file1.yaml',
+     'tests/test_data/file2.yaml',
+     'tests/test_data/expected_result_yaml.txt')])
+def test_generate_diff(file_path1, file_path2, expected_result):
+    diff = generate_diff(file_path1, file_path2)
+    expected = read_file(expected_result).strip()
+    assert diff.strip() == expected
