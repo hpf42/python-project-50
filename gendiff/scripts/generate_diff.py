b29fa2fd36a166a42cc1e6ce732713602c7cbb8f
TalkoDA <azzi2710@gmail.com>
2025-08-03 11:25:19 +0200

    fixed mistakes

--- gendiff/scripts/generate_diff.py
+++ gendiff/scripts/generate_diff.py
@@ -3,7 +3,7 @@ from gendiff.scripts.file_parser import parse_data_from_file
 from gendiff.scripts.find_diff import find_diff
 
 
-def generate_diff(file_1, file_2, formatter='stylish') -> str:
+def generate_diff(file_1, file_2, formatter='stylish'):
     parsed_file_1 = parse_data_from_file(file_1)
     parsed_file_2 = parse_data_from_file(file_2)
     final_diff = find_diff(parsed_file_1, parsed_file_2)
